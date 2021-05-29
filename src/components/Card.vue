3a67f0d90b8acd2155227c836df5d0b2d36fa54d
Tagore <tagore@lastplanr.com>
2021-05-29 10:41:34 -0300

    feat(styles): add basic styles

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/CreateDeck.vue
+++ src/views/CreateDeck.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Card value="AS"/>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-
-export default Vue.extend({
-  name: 'Home',
+<script>
+import Card from '@/components/Card.vue'
+export default {
+  name: 'CreateDeck',
   components: {
-    HelloWorld
+    Card
   }
-})
+}
 </script>
